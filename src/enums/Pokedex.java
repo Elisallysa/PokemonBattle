aa6b6b511be488472700e30a70be3ed57cda1f1d
Elisa <aerodriguez4@gmail.com>
2022-01-11 00:54:13 +0100

    Trying to get Pokedex names in a string with a function

---
+++ src/enums/Pokedex.java
@@ -0,0 +1,15 @@
+package enums;
+
+public enum Pokedex {
+
+	Bulbasaur, Charmander, Squirtle, Butterfree, Pidgeotto, Pikachu, Ekans;
+	
+	
+	public String getName () {
+		
+		String pokeName = this.toString();
+		
+		return pokeName;
+	}
+	
+}
