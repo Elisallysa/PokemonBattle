be6ad927e3f9c2e89d0ae4df47ca5a09c5b0a6cd
Elisa <aerodriguez4@gmail.com>
2022-01-11 00:50:50 +0100

    Added function to get first Pokemon of the list

--- src/models/Equipo.java
+++ src/models/Equipo.java
@@ -24,4 +24,16 @@ public class Equipo {
 	public void addPokemon (Pokemon p) {
 		this.pokEquipo.add(p);
 	}
+	
+	public Pokemon getFirstPokemon () {
+		Pokemon firstPoke = this.pokEquipo.get(0);
+		return firstPoke;
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [pokEquipo=" + pokEquipo + "]";
+	}
+	
+	
 }
