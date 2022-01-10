b167fa2981818465902f10bba6154e115a61704b
Elisa <aerodriguez4@gmail.com>
2022-01-11 00:52:30 +0100

    Selection of Pokemon and creation of Poketeams

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -3,10 +3,10 @@ package main;
 import java.util.Iterator;
 import java.util.Scanner;
 
+import enums.Pokedex;
 import models.AbstractEntrenador;
 import models.Combate;
 import models.EntrenadorHumano;
-import models.Pokedex;
 import models.Pokemon;
 
 public class MainApp {
@@ -19,9 +19,9 @@ public class MainApp {
 		 * Creación de Pokémon:
 		 */
 		
-		Pokemon Bulbasaur = new Pokemon (1, Pokedex.Bulbasaur, null, null, null, 49, 49, 65, 65, 45, null, 0, 0, 10);
-		Pokemon Squirtle = new Pokemon (7, Pokedex.Squirtle, null, null, null, 48, 65, 50, 64, 43, null, 0, 0, 10);
-		Pokemon Charmander = new Pokemon (4, Pokedex.Charmander, null, null, null, 52, 43, 60, 50, 65, null, 0, 0, 10);
+		Pokemon Bulbasaur = new Pokemon (1, Pokedex.Bulbasaur, null, null, null, 49, 49, 65, 65, 45, null, 45, 0, 0, 10);
+		Pokemon Squirtle = new Pokemon (7, Pokedex.Squirtle, null, null, null, 48, 65, 50, 64, 43, null, 44, 0, 0, 10);
+		Pokemon Charmander = new Pokemon (4, Pokedex.Charmander, null, null, null, 52, 43, 60, 50, 65, null, 39, 0, 0, 10);
 		
 		String trainer = "";
 		
@@ -69,12 +69,44 @@ public class MainApp {
 		
 		pokeCombate.setEntrenadorHumano(entrenador1);
 		
-		System.out.println("¡Ya estamos listos!¡Ahora elige hasta tres Pokémon para formar tu equipo!");
-		System.out.println("LISTA POKÉMON: 1. 2. 3. 4.");
+		System.out.println("¡Ya estamos listos! ¿Qué Pokémon vas a elegir?");
+		
+		int option;
+		
+		do {
+			System.out.println("1. Bulbasaur\n2. Charmander\n3. Squirtle\nEscribe el número:");
+			option = Integer.parseInt(s.nextLine());
+		} while (option != 1 && option !=2 && option !=3);
 
-		
-		// entrenador1.getEquipo().addPokemon();
-		
+	
+		switch (option) {
+		case 1:
+			entrenador1.getEquipo().addPokemon(Bulbasaur);
+			if (Math.round(Math.random())==0) {
+				pokeCombate.getEntrenadorCPU().getEquipo().addPokemon(Charmander);	
+			} else {
+				pokeCombate.getEntrenadorCPU().getEquipo().addPokemon(Squirtle);
+			}
+			
+			break;
+		case 2:
+			entrenador1.getEquipo().addPokemon(Charmander);
+			if (Math.round(Math.random())==0) {
+				pokeCombate.getEntrenadorCPU().getEquipo().addPokemon(Bulbasaur);	
+			} else {
+				pokeCombate.getEntrenadorCPU().getEquipo().addPokemon(Squirtle);
+			}
+			break;
+		case 3:
+			entrenador1.getEquipo().addPokemon(Squirtle);
+			if (Math.round(Math.random())==0) {
+				pokeCombate.getEntrenadorCPU().getEquipo().addPokemon(Charmander);	
+			} else {
+				pokeCombate.getEntrenadorCPU().getEquipo().addPokemon(Bulbasaur);
+			}
+		}
+	
+		System.out.println("¡Muy bien!\n"+pokeCombate.getEntrenadorCPU().getNombre()+" va a usar a "+pokeCombate.getEntrenadorCPU().getEquipo().getFirstPokemon().getNombre().getName()+".");
 		
 		
 	}
